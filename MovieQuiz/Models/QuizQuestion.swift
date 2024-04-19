51175c1056802cfe4776a40a018c9c797c689786
marfinv <vmarfin@gmail.com>
2024-04-19 12:40:08 +0500

    Добавлена загрузка данных по API

--- MovieQuiz/Models/QuizQuestion.swift
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -10,7 +10,7 @@ import Foundation
 struct QuizQuestion {
     // строка с названием фильма
     // совпадает с вопросом о рейтинге фильма в Assets
-    let image: String
+    let image: Data
     // строка с вопросом о рейтинге фильма
     let text: String
     // булевое значение (true, false), правильный ответ на вопрос
